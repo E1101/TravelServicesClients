56c7f6c9c62c9733bb7af2ff7dd007247dbabc1b
sajad kashizadeh <s.kashizadeh@gmail.com>
2016-02-10 16:59:58 +0330

    fix Irangardi/HotelServiceOpts.php conflicts

--- Irangardi/HotelServiceOpts.php
+++ Irangardi/HotelServiceOpts.php
@@ -5,7 +5,7 @@ use Poirot\Core\AbstractOptions;
 
 class HotelServiceOpts extends AbstractOptions
 {
-    protected $serverUrl = '‫‪http://94.182.216.5/FarasooMarcopoloHotel/Service.asmx‬‬';
+    protected $serverUrl = 'http://94.182.216.5/FarasooMarcopoloHotel/Service.asmx';
 
     ## account data
     protected $OprCod;
